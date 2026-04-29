e1921bacc6700ea9102b07f10f76b4a38b1c5cd0
Mostafa <mostafazayd2@gmail.com>
2026-04-29 16:34:03 +0300

    remove

--- Front/TakafulAngular/src/app/Features/website/pages/components/website-content/website-content-routing.module.ts
+++ Front/TakafulAngular/src/app/Features/website/pages/components/website-content/website-content-routing.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
-import { TakafulMainComponent } from '../../../takaful-main';
-
-
 const routes: Routes = [
 
   {path:"" , component:HomeComponent},
   {path:"dashboard" , component:DashboardComponent},
-  {path:"takaful" , component:TakafulMainComponent},
   // {path:"AdministrationPolicies" , component:AdministrationPoliciesComponent},
   // {path:"DepartmentDefinition" , component:DepartmentDefinitionComponent},
   // {path: 'treeOrgChart', component: TreeOrgChartComponent},
